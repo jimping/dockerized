be02d9fdc8dba951d58937ed351bb9510267318f
Jonas Imping <info@splintnet.de>
2019-09-18 15:44:10 +0200

    Initial commit

---
+++ mariadb/init.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS nextcloud DEFAULT CHARACTER SET = "utf8";
+CREATE DATABASE IF NOT EXISTS redmine DEFAULT CHARACTER SET = "utf8";
+CREATE DATABASE IF NOT EXISTS suitecrm DEFAULT CHARACTER SET = "utf8";
+CREATE DATABASE IF NOT EXISTS crm DEFAULT CHARACTER SET = "utf8";
+CREATE DATABASE IF NOT EXISTS app DEFAULT CHARACTER SET = "utf8";
+CREATE DATABASE IF NOT EXISTS gitlab DEFAULT CHARACTER SET = "utf8";
+
+CREATE USER IF NOT EXISTS 'nextcloud'@'%' IDENTIFIED BY 'nextcloud';
+CREATE USER IF NOT EXISTS 'redmine'@'%' IDENTIFIED BY 'redmine';
+CREATE USER IF NOT EXISTS 'suitecrm'@'%' IDENTIFIED BY 'suitecrm';
+CREATE USER IF NOT EXISTS 'crm'@'%' IDENTIFIED BY 'crm';
+CREATE USER IF NOT EXISTS 'app'@'%' IDENTIFIED BY 'app';
+CREATE USER IF NOT EXISTS 'gitlab'@'%' IDENTIFIED BY 'gitlab';
+
+GRANT ALL privileges ON `nextcloud`.* TO 'nextcloud'@'%';
+GRANT ALL privileges ON `redmine`.* TO 'redmine'@'%';
+GRANT ALL privileges ON `suitecrm`.* TO 'suitecrm'@'%';
+GRANT ALL privileges ON `crm`.* TO 'crm'@'%';
+GRANT ALL privileges ON `app`.* TO 'app'@'%';
+GRANT ALL privileges ON `gitlab`.* TO 'gitlab'@'%';
